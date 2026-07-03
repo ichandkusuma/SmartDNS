1fd0d865118b5207c9d9e77240fe1807db319172
root <root@SmartDNS.103.151.222.220>
2026-07-04 04:48:40 +0700

    Improve secret management

--- engine/secret.sh
+++ engine/secret.sh
@@ -10,10 +10,22 @@ generate_secret(){
 
     DNSDIST_API_KEY=$(openssl rand -hex 32)
 
-	if [[ -z "${DNSDIST_WEB_PASSWORD:-}" ]]; then
+    if [[ -z "${DNSDIST_WEB_PASSWORD:-}" ]]; then
+        DNSDIST_WEB_PASSWORD=$(openssl rand -base64 18)
+    fi
 
-		DNSDIST_WEB_PASSWORD=$(openssl rand -base64 18)
+    ####################################
+    # Save Secret
+    ####################################
 
-	fi
+    mkdir -p cache
+
+    cat > cache/secret.env <<EOF
+DNSDIST_KEY=${DNSDIST_KEY}
+DNSDIST_API_KEY=${DNSDIST_API_KEY}
+DNSDIST_WEB_PASSWORD=${DNSDIST_WEB_PASSWORD}
+SPOOF_IPV4=${SPOOF_IPV4}
+SPOOF_IPV6=${SPOOF_IPV6}
+EOF
 
 }
