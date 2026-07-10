955854cfee68046d083f7d991848870545bdbae4
Mas iChand Kusuma <ichandkusuma@kakiteng.com>
2026-07-11 02:04:20 +0700

    fix: support fresh installation version detection

--- lib/version.sh
+++ lib/version.sh
@@ -10,10 +10,16 @@ SMARTDNS_VERSION="unknown"
 
 get_version() {
 
-    if [[ -r "$SMARTDNS_HOME/VERSION" ]]; then
-        SMARTDNS_VERSION="$(tr -d '\r\n' < "$SMARTDNS_HOME/VERSION")"
+    local VERSION_PATH
+
+    if [[ -n "${SMARTDNS_HOME:-}" ]]; then
+        VERSION_PATH="${SMARTDNS_HOME}/VERSION"
     else
-        SMARTDNS_VERSION="unknown"
+        VERSION_PATH="${BASE_DIR}/VERSION"
+    fi
+
+    if [[ -r "$VERSION_PATH" ]]; then
+        SMARTDNS_VERSION="$(tr -d '\r\n' < "$VERSION_PATH")"
     fi
 
 }
